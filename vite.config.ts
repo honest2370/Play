e3d17175d470257673d79a16667202234c100444
Honest <honestansah@gmail.com>
2026-05-25 22:52:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
     dedupe: ["react", "react-dom"],
   },
   root: path.resolve(__dirname),
